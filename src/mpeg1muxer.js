78acf14b4001b7d2d948737cc63a10323124db14
zhairuihao <ruihao.zhia@gmail.com>
2019-08-22 23:12:31 +0800

    tianjia shiping geshi kongzhi

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -5,10 +5,12 @@ class Mpeg1Muxer extends EventEmitter {
 
   constructor(options) {
     super(options);
-
+    const scale = !!options.full ? "scale=1280:720,setsar=1:1":"scale=640:360,setsar=1:1";
     this.url = options.url;
 
     this.stream = child_process.spawn('ffmpeg', [
+        // '-thread_queue_size','10240',
+        '-rtsp_transport','tcp',
       '-i',
       this.url,
       '-f',
@@ -16,11 +18,11 @@ class Mpeg1Muxer extends EventEmitter {
       '-codec:v',
       'mpeg1video',
       '-b:v',
-      '2000k',
+      '1000k',
       '-bf',
       '0',
       '-vf',
-      'scale=640:360,setsar=1:1',
+      scale,
       '-codec:a',
       'mp2',
       '-r',
